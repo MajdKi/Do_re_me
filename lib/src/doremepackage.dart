cc2b1157b79cfebac890188c9ecda4e9eeef1e26
*MajdKi* <*majd.kikhia.9@gmail.com*>
2021-12-21 12:34:36 +0200

    color update

--- lib/src/doremepackage.dart
+++ lib/src/doremepackage.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'dart:ui';
 
 class Doreme extends StatefulWidget {
-  const Doreme({ Key? key }) : super(key: key);
+  const Doreme({ Key? key,  this.color }) : super(key: key);
+  final Color? color;
 
   @override
-  _DoremeState createState() => _DoremeState();
+  _DoremeState createState() => _DoremeState(color: color);
 }
 
 class _DoremeState extends State<Doreme> with SingleTickerProviderStateMixin{
+   _DoremeState({this.color});
+   final Color? color;
 
        bool now = false;
   AnimationController? _controller;
